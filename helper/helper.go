e1437767b6b08fafa279392f14850d1a9d49d2c8
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-06-19 15:36:23 +0600

    add: helper package added

---
+++ helper/helper.go
@@ -0,0 +1,11 @@
+package helper
+
+import "strings"
+
+func ValidateUserInput(firstName string, lastName string, emailAddress string, userTickets int, remainingTickets int) (bool, bool, bool) {
+	isValidName := len(firstName) > 2 && len(lastName) > 2
+	isValidEmail := strings.Contains(emailAddress, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicketNumber
+}
