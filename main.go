13985d8c2266126a0c99f636eb1acb8227d3562e
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-06-19 13:37:30 +0600

    logic build for naming other input validation

--- main.go
+++ main.go
@@ -33,23 +33,46 @@ func main() {
 		fmt.Println("Enter how many tickets you want: ")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
+		isValidName := len(firstName) > 2 && len(lastName) > 2
+		isValidEmail := strings.Contains(emailAddress, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		bookings = append(bookings, firstName+" "+lastName)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			bookings = append(bookings, firstName+" "+lastName)
 
-		firstNames := []string{}
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		for _, booking := range bookings {
-			names := strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+			firstNames := []string{}
+
+			for _, booking := range bookings {
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("These are all our bookings: %v\n", bookings)
+
+			fmt.Printf("All bookings first name are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				break
+			}
+		} else {
+
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email addres you entered does not contain @ sign")
+			}
+			if !isValidTicketNumber {
+
+				fmt.Println("number of tickets you entered is wrong")
+			}
 		}
 
-		fmt.Printf("These are all our bookings: %v\n", bookings)
-
-		fmt.Printf("All bookings first name are: %v\n", firstNames)
 	}
 
 }
